5cce535d181e0839e914a81e3e8205e690b10d7f
hpsoar <hpsoar@gmail.com>
2014-08-24 15:59:13 +0800

    Initial Commit

---
+++ OCKit/OCKit.h
@@ -0,0 +1,13 @@
+//
+//  OCKit.h
+//  OCKit
+//
+//  Created by HuangPeng on 8/24/14.
+//  Copyright (c) 2014 Beacon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCKit : NSObject
+
+@end
---
+++ OCKit/OCKit.m
@@ -0,0 +1,13 @@
+//
+//  OCKit.m
+//  OCKit
+//
+//  Created by HuangPeng on 8/24/14.
+//  Copyright (c) 2014 Beacon. All rights reserved.
+//
+
+#import "OCKit.h"
+
+@implementation OCKit
+
+@end
